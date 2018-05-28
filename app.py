2d0dd9638294da243142db82f82c4fce076af655
lucky-hee <gobaksa4@naver.com>
2018-05-28 11:06:57 +0900

    9. Adding A Bootstrap Theme to Flask

--- app.py
+++ app.py
@@ -51,7 +51,25 @@ def create_user():
 # Views
 @app.route('/')
 def index():
-    return render_template('add_user.html', myUser = myUser, oneItem = oneItem)
+    return render_template('index.html')
+
+@app.route('/index')
+def index2():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/post')
+def post():
+    return render_template('post.html')
 
 #6. Dynamic URL Querying
 @app.route('/profile/<email>')
